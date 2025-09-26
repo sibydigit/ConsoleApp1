0a16ddf3a5035b18133a5eae38ad8f94b3ed4b73
sibydigit <sibydigit@gmail.com>
2025-09-26 10:25:50 +0300

    add class file

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@ using System.Transactions;
 using System.Collections.Generic;
 namespace csharp {
 
+
     class program {
+
+
+        class B
+        {
+            private int a;
+            public int b;
+            protected int c;
+
+            public void display()
+            {
+                Console.WriteLine("Hello world");
+            }
+            
+
+        }
 
         public static void Main(string[] args)
         {
@@ -12,10 +28,10 @@ namespace csharp {
            
             // Run main function parameters
             // Command Line Arguments
-            foreach(string i in args)
-            {
-                Console.WriteLine(i);
-            }
+            //foreach(string i in args)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
             //Access Modifiers
 
@@ -27,6 +43,10 @@ namespace csharp {
             //accessed from anywhwre inside the project and also outside
 
             //private is used to define that the class can access only with in the same class
+
+            B clsobj = new B();
+            clsobj.display();
+
 
          }
         }
