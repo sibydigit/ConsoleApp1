1b5b653e06031aac280b4a564419644912a286d1
sibydigit <sibydigit@gmail.com>
2025-09-26 09:45:13 +0300

    Files Saved

---
+++ Prgm2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Prgm2
+    {
+        //Console.WriteLine("Hello, World!");
+
+        //ArrayList x = new ArrayList();
+        //x.Add("Rasool");
+        //x.Add("Anish");
+        //x.Add(true);
+        //x.Add(12.33);
+        //foreach (dynamic s in x)
+        //{
+        //    Console.WriteLine(s);   
+        //}
+
+        //Insret method : to insert a new value in postion in array
+
+        //Console.ReadLine();
+        //Console.WriteLine("Enter the postion of the line where the value is inerted :");
+        //int j = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter the value is inerted :");
+        //var c = Console.ReadLine();
+        //x.Insert(j, c);
+        //foreach (var i in x)
+        //{
+        //    Console.WriteLine("-----");
+        //    Console.WriteLine(i);
+        //}
+
+        //var y = new ArrayList() { "Siby", "Anu", "insu", "Bobby" };
+        //x.AddRange(y);
+        //foreach (var i in x)
+        //{
+        //    Console.WriteLine("-----");
+        //    Console.WriteLine(i);
+        //}
+
+        //var xr=new ArrayList() {12,240,100,140,114,235,516,450};
+        //xr.Sort();
+        //foreach (var i in xr)
+        //{
+        //    Console.WriteLine("-----");
+        //    Console.WriteLine(i);
+        //}
+        //xr.Reverse ();
+        //Console.WriteLine("In Reverse \n");
+        //foreach (var i in xr)
+        //{
+        //    Console.WriteLine("-----");
+        //    Console.WriteLine(i);
+        //}
+
+
+        //List<string> SS = new List<string>();
+        //Console.WriteLine("Enter the size of the list:");
+        //int size = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 0; i < size; i++)
+        //{
+        //   SS.Add(Console.ReadLine());
+
+        //}
+        //SS.Insert(0, "Siby");
+        //foreach (string i in SS)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //Sorted List
+        //SortedList<int,string> ss= new SortedList<int,string>();
+
+        //ss.Add(0,"Apple");
+        //ss.Add(1,"Orangle");
+        //ss.Add(2,"Mangoes");
+
+        //foreach(KeyValuePair<int,string>i in ss)
+        //    {
+        //    Console.WriteLine(i.Key+" "+i.Value);
+
+        //}
+
+
+
+        //Hash Tables
+        //Hashtable abc=new Hashtable();
+        //abc.Add("a", "abncc");
+        //abc.Add("1", "Appled");
+        //abc.Add("#", "Mathe3w");
+
+        //foreach (DictionaryEntry e in abc)
+        //{
+        //    Console.WriteLine(e.Key+" "+ e.Value);
+        //}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Transactions;
+using System.Collections.Generic;
+namespace csharp {
+
+    class program {
+
+        public static void Main(string[] args)
+        {
+
+           
+            // Run main function parameters
+            // Command Line Arguments
+            foreach(string i in args)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Access Modifiers
+
+            // Access Modifiers are keywords that defiene the accessability of the members of he class.
+            //it defines the access limitations of each elements
+            // public, protected,private,internal
+            //By default is private  and in case of class is internal
+            //public access modifier is used to define the class or its members can 
+            //accessed from anywhwre inside the project and also outside
+
+            //private is used to define that the class can access only with in the same class
+
+         }
+        }
+}
+
+
+
+
